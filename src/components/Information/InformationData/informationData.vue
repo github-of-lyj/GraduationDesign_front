6cde01041407b183722c74cdb786fbeecc319ff6
Alex <Alex@lyj.com>
2024-02-20 00:02:48 +0800

    编写了上传资料的前端和后端之间的联动逻辑

--- src/components/Information/InformationData/informationData.vue
+++ src/components/Information/InformationData/informationData.vue
@@ -2,40 +2,52 @@
   <div>
     <div id="upload">
       <el-upload
-        class="avatar-uploader"
-        :action="`http://localhost:8080/user/file/uploadUserAvatar?userID=${userID}`"
-        :show-file-list="false"
-        :on-success="handleAvatarSuccess"
-        :before-upload="beforeAvatarUpload"
+        class="upload-demo"
+        action="http://localhost:8080/user/uploadfile/upload?userID=1"
+        :before-upload="beforeFileUpload"
+        :on-success="onFileUploadSuccess"
       >
-        <img v-if="imageUrl" :src="imageUrl" class="avatar" />
-        <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        <el-button size="small" type="primary">点击上传</el-button>
+        <div slot="tip" class="el-upload__tip">
+          只能上传zip文件，且不超过400MB
+        </div>
       </el-upload>
     </div>
     <div id="informationData">
       <div>
         <tr>
-          <th id="type">类别</th>
           <th id="name">名称</th>
-          <th id="link">链接</th>
           <th id="size">
             大小<i class="el-icon-d-caret" @click="sortBySize"></i>
           </th>
+          <th id="name">上传用户</th>
           <th id="date">
             日期<i class="el-icon-d-caret" @click="sortByDate"></i>
           </th>
+          <th id="link">下载</th>
           <th id="download">
             下载量<i class="el-icon-d-caret" @click="sortByDownload"></i>
           </th>
         </tr>
         <tbody>
-          <tr class="default">
-            <td id="type">类别</td>
-            <td id="name">名称</td>
-            <td id="link">链接</td>
-            <td id="size">大小</td>
-            <td id="date">日期</td>
-            <td id="download">下载量</td>
+          <tr class="default" v-for="UploadFile in UploadFileList" :key="UploadFile.uploadfileID">
+            <td id="name">{{getFileName(UploadFile.uploadfileName)}}.zip</td>
+            <td id="size">{{UploadFile.uploadfileSize}}KB</td>
+            <th id="name">
+              <router-link
+                :to="{
+                  name: 'User',
+                  params: {
+                    userid: UploadFile.userID,
+                },
+              }"
+              >
+                {{UploadFile.userName}}
+              </router-link>
+            </th>
+            <td id="date">{{UploadFile.uploadDate}}</td>
+            <td id="link"><span class="el-icon-download" style="cursor: pointer;color:red;" @click="download"></span></td>
+            <td id="download">{{UploadFile.downloadCounts}}</td>
           </tr>
         </tbody>
       </div>
@@ -44,39 +56,76 @@
 </template>
 
 <script>
+import axios from 'axios'
+import pubsub from 'pubsub-js'
 export default {
   data() {
     return {
-      imageUrl: "",
-      userID: 2,
+      UploadFileList:[]
     };
   },
   methods: {
     sortBySize() {
-      console.log("按照 size 排序");
+      this.UploadFileList.sort((a,b)=>{
+        return parseInt(b.uploadfileSize) - parseInt(a.uploadfileSize)
+      })
+      this.$message({
+        message: '按照文件大小从大到小排列',
+        type: 'info'
+      })
     },
     sortByDate() {
-      console.log("按照 date 排序");
+      this.UploadFileList.sort((a,b)=>{
+        const dateA = new Date(a.uploadDate);
+        const dateB = new Date(b.uploadDate);
+        return dateB - dateA
+      })
+      this.$message({
+        message: '按照日期从晚到早排列',
+        type: 'info'
+      })
     },
     sortByDownload() {
-      console.log("按照 download 排序");
+      this.UploadFileList.sort((a,b)=>{
+        return b.downloadCounts - a.downloadCounts
+      })
+      this.$message({
+        message: '按照下载量从高到低排序',
+        type: 'info'
+      })
     },
+    getFileName(fileName){
+      const separatorIndex = fileName.indexOf("-")
+      return fileName.substring(0, separatorIndex)
+    },
+    beforeFileUpload(file) {
+      const isZip = file.type === "application/x-zip-compressed";
 
-    handleAvatarSuccess(res, file) {
-      console.log(file);
-      this.imageUrl = URL.createObjectURL(file.raw);
+      if (!isZip) {
+        this.$message.error("上传文件只能是zip格式!");
+      }
+      return isZip
     },
-    beforeAvatarUpload(file) {
-      this.imageUrl = URL.createObjectURL(file);
-      // if (!isJPG) {
-      //   this.$message.error('上传头像图片只能是 JPG 格式!');
-      // }
-      // if (!isLt2M) {
-      //   this.$message.error('上传头像图片大小不能超过 2MB!');
-      // }
-      return false;
+    onFileUploadSuccess(){
+      pubsub.publish('getUploadFile')
     },
+    download(){
+      console.log('开始下载')
+    }
   },
+  mounted(){
+    pubsub.subscribe('getUploadFile',() => {
+      axios.get('http://localhost:8080/user/uploadfile/selectAllUploadFile').then(
+        (response) => {
+          this.UploadFileList = response.data
+        },
+        (error) => {
+          console.log(error)
+        }
+      )
+    })
+    pubsub.publish('getUploadFile')
+  }
 };
 </script>
 
@@ -118,7 +167,7 @@ export default {
 }
 #name {
   width: 300px;
-  display: flex;
+  text-align: center;
 }
 #link {
   width: 70px;
@@ -129,7 +178,7 @@ export default {
   text-align: center;
 }
 #date {
-  width: 140px;
+  width: 300px;
   text-align: center;
 }
 #download {
@@ -150,5 +199,4 @@ td {
 i:hover {
   cursor: pointer;
 }
-
 </style>
